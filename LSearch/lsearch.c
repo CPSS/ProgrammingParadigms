749cb8ba8542c7fd9e04cac2e244405116c4eb80
Guangzhan <fuzhan317@gmail.com>
2014-12-25 20:24:30 +0800

    Create lsearch.c
    This a function that returns the address within the address within the array of the matching element.

---
+++ LSearch/lsearch.c
@@ -0,0 +1,11 @@
+void *lserch(void *key, void *base, int n, int elemSize)
+{
+  for(int i=0; i<n; i++){
+    void *elemAddr = (char*)base + i * elemSize;
+    if(memcmp(key, elemAddr, elemSize) == 0){
+      return elemAddr;
+    }
+    return NULL;
+  }
+}
+
